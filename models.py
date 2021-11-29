58b47441257399e957eed35d91cb338d623f1ab5
Luca Chang <82618674+LucaOnline@users.noreply.github.com>
2021-11-28 18:09:36 -0800

    Set up basic mask model

--- models.py
+++ models.py
@@ -1,8 +1,35 @@
 from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, DepthwiseConv2D, MaxPooling2D, BatchNormalization, Flatten, Dense
 
 
-def create_mask_model() -> Sequential:
-    model = Sequential()
+def create_mask_model(num_classes: int) -> Sequential:
+    model = Sequential([
+        Conv2D(64, (3, 3), activation="relu", kernel_initializer="he_normal"),
+        BatchNormalization(),
+        Conv2D(64, (3, 3), activation="relu", kernel_initializer="he_normal"),
+        BatchNormalization(),
+        MaxPooling2D(),
+
+        Conv2D(32, (3, 3), activation="relu", kernel_initializer="he_normal"),
+        BatchNormalization(),
+        DepthwiseConv2D(16, (3, 3), padding="same",
+                        kernel_initializer="he_normal"),
+        BatchNormalization(),
+        MaxPooling2D(),
+
+        Flatten(),
+        Dense(64, activation="relu", kernel_initializer="he_normal"),
+        BatchNormalization(),
+        Dense(num_classes, activation="softmax",
+              kernel_initializer="he_normal"),
+    ])
+
+    model.compile(
+        loss="sparse_categorical_crossentropy",
+        optimizer="adam",
+        metrics=["accuracy"],
+    )
+
     return model
 
 
--- train_mask.py
+++ train_mask.py
@@ -1,11 +1,24 @@
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
+from xcaptcha.defaults import CHARSET_ALPHANUMERIC
+
 from data import build_dataset, Mode
 from models import create_mask_model
 
 
 def main():
     ds = build_dataset(Mode.Masks)
-    print(ds.take(1))
-    model = create_mask_model()
+    val_ds = build_dataset(Mode.Masks)
+
+    model = create_mask_model(len(CHARSET_ALPHANUMERIC))
+
+    cb = [
+        EarlyStopping(monitor="val_loss", mode="min", patience=10, verbose=1),
+        ModelCheckpoint("masks.hdf5", monitor="val_loss",
+                        save_best_only=True, verbose=1),
+        TensorBoard(),
+    ]
+
+    model.fit(ds, validation_data=val_ds, epochs=10, callbacks=cb, verbose=1)
 
 
 if __name__ == "__main__":
