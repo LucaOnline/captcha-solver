10d5dd0dd753f80cb908496f93149b0c328d8ed2
Luca Chang <82618674+LucaOnline@users.noreply.github.com>
2021-12-04 15:51:14 -0800

    Create masknet sample image generator

--- make_graphs.py
+++ make_graphs.py
@@ -1,3 +1,4 @@
+import cv2 as cv
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import roc_curve
@@ -6,6 +7,31 @@ from tensorflow.keras.models import load_model
 from xcaptcha.defaults import CHARSET_ALPHANUMERIC
 
 from data import build_dataset, Mode
+
+
+def sample_mask_predictions():
+    dims = (75, 150)
+
+    ds = build_dataset(dims, 1, Mode.Masks)
+    model = load_model("masks.hdf5")
+
+    img, mask = list(ds.batch(1).take(1).as_numpy_iterator())[0]
+    predicted_mask = model.predict(img)
+
+    img = np.reshape(img, dims)
+    mask = np.reshape(mask, dims)
+    predicted_mask = np.reshape(predicted_mask, dims)
+
+    cv.imshow("Image", img)
+    cv.waitKey(0)
+    cv.imshow("Mask", mask)
+    cv.waitKey(0)
+    cv.imshow("Predicted Mask", predicted_mask)
+    cv.waitKey(0)
+
+    cv.imwrite("out/masks_image.png", img)
+    cv.imwrite("out/masks_label.png", mask)
+    cv.imwrite("out/masks_pred.png", predicted_mask)
 
 
 def make_roc_curve():
@@ -48,4 +74,4 @@ def make_roc_curve():
 
 
 if __name__ == "__main__":
-    make_roc_curve()
+    sample_mask_predictions()
